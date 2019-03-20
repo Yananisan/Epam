8303761fe4222e403e2216b0d07935bab284fd94
Yana Ivanovskaya <yanaivanovskaya@mail.ru>
2019-03-21 01:49:53 +0300

    QuickSort
    QuickSort full program with one test.
    Write test on ArgumentNullException & ArgumentException. Write XML documentation (Next commit).

---
+++ NET.S.2019.Ivanovskaya.01/QuickSort/ClassLibrary2/QuickSortTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace QuickSort.Test
+{
+    public class Test
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] array = { 24, 16, 54 };
+            QuickSort.quicksort(array);
+            Assert.AreEqual(new int[] { 16, 24, 54 }, array);
+        }
+    }
+}
