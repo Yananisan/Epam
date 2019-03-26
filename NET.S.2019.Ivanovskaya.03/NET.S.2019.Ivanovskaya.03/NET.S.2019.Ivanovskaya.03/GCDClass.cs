c1a9c584c9c610e3c698ae6a2666aba783794a5c
Yana Ivanovskaya <yanaivanovskaya@mail.ru>
2019-03-26 23:12:03 +0300

    Add readme + Day3_Task1

---
+++ NET.S.2019.Ivanovskaya.03/NET.S.2019.Ivanovskaya.03/NET.S.2019.Ivanovskaya.03/GCDClass.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NET.S._2019.Ivanovskaya._03
+{
+    public class GCDClass
+    {
+        public static int GCD(out TimeSpan time, params int[] array)
+        {
+            time = new TimeSpan();
+            Stopwatch stopwatch = new Stopwatch();
+
+            if (array == null) throw new ArgumentNullException();
+            if (array.Length < 1) throw new ArgumentException();
+            if (array.Length == 1) return array[0];
+            
+            stopwatch.Start();
+
+            int result = array[0];
+            for (int i = 1; i < array.Length; i++)
+                result = BaseGCD(array[i], result);
+
+            stopwatch.Stop();
+            time = stopwatch.Elapsed;
+
+            Console.WriteLine("TimeSpan of given number: {0:00}:{1:00}:{2:00}.{3:00}",
+            time.Hours, time.Minutes, time.Seconds, time.Milliseconds / 10);
+
+            return result;
+        }
+
+        private static int BaseGCD(int a, int b)
+        {
+            if (a < 0) a *= -1;
+            if (b < 0) b *= -1;
+
+            if (a == 0)
+                return b;
+            return BaseGCD(b % a, a);
+        }
+
+        public static int BinaryGCD(out TimeSpan time, params int[] array)
+        {
+            time = new TimeSpan();
+            Stopwatch stopwatch = new Stopwatch();
+
+            if (array == null) throw new ArgumentNullException();
+            if (array.Length < 1) throw new ArgumentException();
+            if (array.Length == 1) return array[0];
+
+            stopwatch.Start();
+
+            int result = array[0];
+            for (int i = 1; i < array.Length; i++)
+                result = BinaryBaseGCD(array[i], result);
+
+            stopwatch.Stop();
+            time = stopwatch.Elapsed;
+
+            Console.WriteLine("TimeSpan of given number: {0:00}:{1:00}:{2:00}.{3:00}",
+            time.Hours, time.Minutes, time.Seconds, time.Milliseconds / 10);
+
+            return result;
+        }
+
+        private static int BinaryBaseGCD(int a, int b)
+        {
+            if (a < 0) a *= -1;
+            if (b < 0) b *= -1;
+
+            if (a == 0)
+                return b;
+            if (b == 0)
+                return a;
+
+            int k = 1;
+            while ((a != 0) && (b != 0))
+            {
+                while ((a % 2 == 0) && (b % 2 == 0))
+                {
+                    a >>= 1;
+                    b >>= 1;
+                    k <<= 1;
+                }
+
+                if (a >= b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+
+            return b * k;
+        }
+    }
+}
---
+++ NET.S.2019.Ivanovskaya.03/NET.S.2019.Ivanovskaya.03/NET.S.2019.Ivanovskaya.03/Tests/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NET.S._2019.Ivanovskaya._03.Tests
+{
+    class Tests
+    {
+        #region Euclidian GCD
+
+        [TestCase(7, 14, 49, ExpectedResult = 7)]
+        [TestCase(16, 144, 32, 56, ExpectedResult = 8)]
+        [TestCase(12, ExpectedResult = 12)]
+        [TestCase(-33, 11, 110, ExpectedResult = 11)]
+        [TestCase(10, 20, 30, 50, 100, ExpectedResult = 10)]
+        [TestCase(0, 13, 26, ExpectedResult = 13)]
+        public int GCDTests(params int[] array) =>
+            GCDClass.GCD(out TimeSpan time, array);
+
+        [Test]
+        public void CheckOnNull() => Assert.Throws<ArgumentNullException>(() =>
+        GCDClass.GCD(out TimeSpan time, null));
+
+        [Test]
+        public void CheckOnEmpty() => Assert.Throws<ArgumentException>(() =>
+        GCDClass.GCD(out TimeSpan time, new int[] { }));
+
+        #endregion
+
+        #region Stein's GCD
+
+        [TestCase(7, 14, 49, ExpectedResult = 7)]
+        [TestCase(16, 144, 32, 56, ExpectedResult = 8)]
+        [TestCase(12, ExpectedResult = 12)]
+        [TestCase(-33, 11, 110, ExpectedResult = 11)]
+        [TestCase(10, 20, 30, 50, 100, ExpectedResult = 10)]
+        [TestCase(0, 13, 26, ExpectedResult = 13)]
+        public int BinaryGCDTests(params int[] array) =>
+        GCDClass.BinaryGCD(out TimeSpan time, array);
+
+        [Test]
+        public void CheckBinaryOnNull() => Assert.Throws<ArgumentNullException>(() =>
+        GCDClass.BinaryGCD(out TimeSpan time, null));
+
+        [Test]
+        public void CheckBinaryOnEmpty() => Assert.Throws<ArgumentException>(() =>
+        GCDClass.BinaryGCD(out TimeSpan time, new int[] { }));
+
+        #endregion
+    }
+}
