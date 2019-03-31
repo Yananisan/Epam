30ff2a6f2afdb1906517e7c7b516430e05b6b125
Yana Ivanovskaya <yanaivanovskaya@mail.ru>
2019-03-31 16:57:50 +0300

    Override methods, Constructor, Indexer

---
+++ NET.S.2019.Ivanovskaya.05/NET.S.2019.Ivanovskaya.05/Polynomial.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Text;
+
+namespace NET.S._2019.Ivanovskaya._05
+{
+    public class Polynomial: IDisposable, ICloneable
+    {
+
+        private readonly double[] _coefficients;
+
+        #region Clone
+
+        object ICloneable.Clone() => Clone();
+
+        public Polynomial Clone()
+        {
+            return new Polynomial(_coefficients);
+        }
+
+        #endregion
+
+        public Polynomial(params double[] coefficients)
+        {
+            if (coefficients == null)
+            {
+                throw new ArgumentNullException($"{nameof(coefficients)} can't be equal to null.");
+            }
+
+            if (coefficients.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(coefficients)} can't be empty!");
+            }
+
+            _coefficients = coefficients;
+        }
+
+        public int Length
+        {
+            get
+            {
+                return _coefficients.Length;
+            }
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > Length)
+                    throw new ArgumentOutOfRangeException($"{nameof(index)} is out of range!");
+
+                return _coefficients[index];
+            }
+
+            private set
+            {
+                if (index < 0 || index > Length)
+                    throw new ArgumentOutOfRangeException($"{nameof(index)} is out of range!");
+
+                _coefficients[index] = value;
+            }
+        }
+
+        public int Power
+        {
+            get
+            {
+                return _coefficients.Length;
+            }
+        }
+
+        #region Overriding Object methods: ToString, GetHashCode, Equals, Finalize
+
+        #region ToString
+
+        public override string ToString()
+        {
+            if (this == null)
+            {
+                throw new ArgumentNullException($"Object can't be equal to null!");
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = this.Power - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    result.Append(_coefficients[i]);
+                }
+                else
+                {
+                    if (i != 1)
+                    {
+                        result.Append(_coefficients[i] + "x^" + i + " + ");
+                    }
+                    else
+                    {
+                        if (this[i] > 0)
+                        {
+                            result.Append(_coefficients[i] + "x" + " + ");
+                        }
+                        else
+                        {
+                            result.Append(_coefficients[i] + "x");
+                        }
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        #endregion
+
+        #region GetHashCode
+
+        public override int GetHashCode()
+        {
+            int result = 0;
+            foreach (double value in _coefficients)
+            {
+                result += ShiftAndWrap(value.GetHashCode(), 2);
+            }
+            return result;
+        }
+
+        public int ShiftAndWrap(int value, int positions)
+        {
+            positions = positions & 0x1F;
+
+            // Save the existing bit pattern, but interpret it as an unsigned integer.
+            uint number = BitConverter.ToUInt32(BitConverter.GetBytes(value), 0);
+            // Preserve the bits to be discarded.
+            uint wrapped = number >> (32 - positions);
+            // Shift and wrap the discarded bits.
+            return BitConverter.ToInt32(BitConverter.GetBytes((number << positions) | wrapped), 0);
+        }
+
+        #endregion
+
+        #region Equals
+
+        public bool Equals(Polynomial other)
+        {
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (this is null || other is null)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < other.Length; i++)
+            {
+                if (other[i] != _coefficients[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (this is null || obj is null)
+            {
+                return false;
+            }
+
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return Equals((Polynomial)obj);
+        }
+
+        #endregion
+
+        #region Finalize(Dispose)
+
+        private bool _isDisposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool flag)
+        {
+            if (_isDisposed) return;
+            if (flag) GC.SuppressFinalize(this);
+        }
+
+        ~Polynomial()
+        {
+            this.Dispose(false);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ NET.S.2019.Ivanovskaya.05/NET.S.2019.Ivanovskaya.05/Tests/Tests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace NET.S._2019.Ivanovskaya._05.Tests
+{
+    class Tests
+    {
+
+        #region ToString
+
+        [TestCase]
+        public void Polynomial_ToString_IsCorrect()
+        {
+            Polynomial test = new Polynomial(1, 2, 3);
+            string expected = "3x^2 + 2x + 1";
+
+            string result = test.ToString();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase]
+        public void Polynomial_ToString_IsNotCorrect()
+        {
+            Polynomial test = new Polynomial(1, 2, 3);
+            string expected = "9x^2 + 5x";
+
+            string result = test.ToString();
+            Assert.AreNotEqual(expected, result);
+        }
+
+        [TestCase]
+        public void Polynomial_ToString_NullReferenceException()
+        {
+            Polynomial test = null;
+            Assert.Throws<NullReferenceException>(() => test.ToString());
+        }
+
+        #endregion
+
+        #region Equal
+
+        [TestCase]
+        public void Polynomial_EqualOperator_IsCorrect()
+        {
+            Polynomial poly1 = new Polynomial(1, 2);
+            Polynomial poly2 = poly1;
+
+            bool expected = true;
+            bool actual = (poly1 == poly2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void Polynomial_NotEqualOperator_IsCorrect()
+        {
+            Polynomial poly1 = new Polynomial(1, 2);
+            Polynomial poly2 = poly1;
+
+            bool expected = false;
+            bool actual = (poly1 != poly2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void Polynomial_Equals_IsEqual()
+        {
+            Polynomial obj = new Polynomial(1, 2, 3);
+            bool expected = true;
+            bool actual = obj.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void Polynomial_EqualsObject_IsCorrect()
+        {
+            Polynomial poly1 = new Polynomial(1, 2);
+            object poly2 = new Polynomial(1, 2);
+
+            bool expected = true;
+            bool actual = poly1.Equals(poly2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void Polynomial_Equals_IsNotEqual()
+        {
+            Polynomial lhs = new Polynomial(1, 2, 3);
+            Polynomial rhs = new Polynomial(2, 3, 4);
+            bool expected = false;
+
+            bool result = lhs.Equals(rhs);
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+
+        #region Constructor
+
+        [TestCase]
+        public void PolynomialConstructor_NullArg_ArgumentNullException()
+            => Assert.Throws<ArgumentNullException>(() => new Polynomial(null));
+
+        [TestCase]
+        public void PolynomialConstructor_EmptyArg_ArgumentException()
+            => Assert.Throws<ArgumentException>(() => new Polynomial(new double[] { }));
+
+        #endregion
+
+        #region Indexer
+
+        [TestCase]
+        public void PolynomialIndexer_IsCorrect()
+        {
+            Polynomial poly = new Polynomial(new double[] { 1, 2, 3 });
+            double expected = 2;
+
+            double actual = poly[1];
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void PolynomialIndexer_ArgumentOutOfRangeException()
+        {
+            Polynomial poly = new Polynomial(new double[] { 1, 2, 3 });
+            double temp = 0;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => temp = poly[10]);
+        }
+
+        #endregion
+    }
+}
